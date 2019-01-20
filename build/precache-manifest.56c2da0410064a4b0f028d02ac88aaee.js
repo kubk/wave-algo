fd2ba8f7b2dc4b0cea214604f437eab9848b2bb7
kubk <77gorbachevm@gmail.com>
2019-01-20 22:38:57 +0300

    build

---
+++ build/precache-manifest.56c2da0410064a4b0f028d02ac88aaee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34621adbbb9d87fe4e48",
+    "url": "/static/js/main.34621adb.chunk.js"
+  },
+  {
+    "revision": "b898cd86158b6272643a",
+    "url": "/static/js/1.b898cd86.chunk.js"
+  },
+  {
+    "revision": "34621adbbb9d87fe4e48",
+    "url": "/static/css/main.acee54f8.chunk.css"
+  },
+  {
+    "revision": "76cdea233652557c9e99096ed5ac2b0f",
+    "url": "/index.html"
+  }
+];
